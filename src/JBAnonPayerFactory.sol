dcb3e505ac84db2699b9286b21bb6f2864c7443f
drgorillamd <dr_gorilla@protonmail.com>
2022-11-09 17:55:41 +0100

    helper: get future address

--- src/JBAnonPayerFactory.sol
+++ src/JBAnonPayerFactory.sol
@@ -27,6 +27,20 @@ contract JBAnonPayerFactory {
     }
 
     /**
+        @notice Helper to get the address where to send fund, prior to anon payer deployment
+    */
+    function getTargetAddress(uint256 _projectId, address _sender) external view returns(address _target) {
+        bytes memory _creationBytecode = type(JBAnonPayer).creationCode;
+        bytes32 _salt = keccak256(abi.encode(_projectId, _sender));
+
+        bytes32 hash = keccak256(
+            abi.encodePacked(bytes1(0xff), address(this), _salt, keccak256(_creationBytecode))
+        );
+
+        return address(uint160(uint256(hash)));
+    }
+
+    /**
         @notice Deploy an anon payer on the address corresponding to a given project id, for a given sender,
                 and trigger the call to pay() on the corresponding terminal.
 
@@ -38,6 +52,6 @@ contract JBAnonPayerFactory {
 
         JBAnonPayer _minion = new JBAnonPayer{salt: _salt}();
 
-        _minion.pay(directory, _projectId, _token);
+        _minion.pay(directory, _projectId, _token, _sender);
     }
 }
